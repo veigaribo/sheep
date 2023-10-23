8edfca024be76fc8d9d467e0a7a2eb5fda718657
Gabriel Lopes Veiga <veigo@veigo.dev>
2023-10-23 16:30:41 -0300

    Sanitize multiplayer input fields

--- menus/multiplayer/host_or_connect/connect/connect.gd
+++ menus/multiplayer/host_or_connect/connect/connect.gd
@@ -7,11 +7,11 @@ var player_name: String
 
 @onready var lobby_scene := load(lobby_scene_path)
 
-@onready var message_label = $CenterContainer/VBoxContainer/MessageLabel as Label
+@onready var message_label = $ScrollContainer/CenterContainer/VBoxContainer/MessageLabel as Label
 
-@onready var name_edit := $CenterContainer/VBoxContainer/NameEdit as LineEdit
-@onready var ip_addr_edit := $CenterContainer/VBoxContainer/IpAddrEdit as LineEdit
-@onready var port_edit := $CenterContainer/VBoxContainer/PortEdit as LineEdit
+@onready var name_edit := $ScrollContainer/CenterContainer/VBoxContainer/NameEdit as LineEdit
+@onready var ip_addr_edit := $ScrollContainer/CenterContainer/VBoxContainer/IpAddrEdit as LineEdit
+@onready var port_edit := $ScrollContainer/CenterContainer/VBoxContainer/PortEdit as LineEdit
 
 
 func _ready():
@@ -20,50 +20,112 @@ func _ready():
 
 
 func _on_ok_pressed():
-	message_label.set_visible(false)
+	_clear_message()
 	
 	# Close any possible hanging attempted connections
 	multiplayer.get_multiplayer_peer().close()
 	
-	var peer = ENetMultiplayerPeer.new()
-	
 	# All sanitization should go here
-	var ip_address := _get_ip_addr()
-	var port := _get_port()
-	player_name = _get_player_name()
+	var address = _get_addr()
+	var port = _get_port()
+	var maybe_player_name = _get_player_name()
 	
-	peer.create_client(ip_address, port)
+	if null in [address, port, maybe_player_name]:
+		return
+	
+	player_name = maybe_player_name
+	
+	var peer = ENetMultiplayerPeer.new()
+	peer.create_client(address, port)
+	
 	multiplayer.set_multiplayer_peer(peer)
+	
+	_display_message("Connecting...")
 
 
-func _get_ip_addr() -> String:
-	var ip_address := ip_addr_edit.get_text()
-	return ip_address
-
-
-func _get_port() -> int:
-	var port := int(port_edit.get_text())
-	return port
-
-
-func _get_player_name() -> String:
-	var name := name_edit.get_text()
+func _get_player_name():
+	var name := name_edit.get_text().strip_edges()
+	
+	if name.length() == 0:
+		_display_message("Name should not be empty.")
+		return null
+	
+	if name.length() > 20:
+		_display_message("Please keep name at most 20 characters long.")
+		return null
+	
 	return name
 
 
-func _go_to_lobby():
-	var player_id := multiplayer.get_unique_id()
-	var player := Player.new(player_id, player_name)
-	multiplayer_data.self_player = player
+func _get_addr():
+	var address := ip_addr_edit.get_text().strip_edges()
 	
+	if address.length() == 0:
+		_display_message("Address should not be empty.")
+		return null
+	
+	if address.length() > 253:
+		_display_message("Address length should not exceed 253.")
+		return null
+	
+	return address
+
+
+func _get_port():
+	var s_port := port_edit.get_text().strip_edges()
+	
+	if not s_port.is_valid_int():
+		_display_message("Port should be a whole number.")
+		return null
+	
+	var port = int(s_port)
+	
+	if port <= 0:
+		_display_message("Port should be a positive number.")
+		return null
+	
+	if port > 65535:
+		_display_message("Port should not exceed 65535.")
+		return null
+	
+	return port
+
+
+func _go_to_lobby():
 	get_tree().change_scene_to_file(lobby_scene_path)
 
 
 func _on_connected():
+	var player_id := multiplayer.get_unique_id()
+	var player := Player.new(player_id, player_name)
+	multiplayer_data.self_player = player
+	
 	_go_to_lobby()
 
 
 func _on_not_connected():
 	# Takes a while to happen
-	message_label.set_text("Could not connect.")
-	message_label.set_visible(true)
+	_clear_message()
+	_display_message("Could not connect.")
+
+
+func _clear_message():
+	message_label.set_visible(false)
+	message_label.set_text("")
+
+
+func _display_message(message: String):
+	var current_text = message_label.get_text()
+	
+	if current_text.is_empty():
+		message_label.set_text(message)
+		message_label.set_visible(true)
+	else:
+		message_label.set_text(current_text + "\n" + message)
+	
+	return
+
+
+func _on_return_pressed():
+	# Close any possible hanging attempted connections
+	multiplayer.get_multiplayer_peer().close()
--- menus/multiplayer/host_or_connect/host/host.gd
+++ menus/multiplayer/host_or_connect/host/host.gd
@@ -11,59 +11,108 @@ var player_name: String
 
 @onready var name_edit := $ScrollContainer/CenterContainer/VBoxContainer/NameEdit as LineEdit
 @onready var port_edit := $ScrollContainer/CenterContainer/VBoxContainer/PortEdit as LineEdit
+@onready var max_players_edit := $ScrollContainer/CenterContainer/VBoxContainer/MaxPlayersEdit as LineEdit
 
 @onready var ip_addr_edit := $ScrollContainer/CenterContainer/VBoxContainer/AdvancedContainer/IpAddrEdit as LineEdit
-@onready var max_players_edit := $ScrollContainer/CenterContainer/VBoxContainer/AdvancedContainer/MaxPlayersEdit as LineEdit
 
 
 func _on_ok_pressed():
-	message_label.set_visible(false)
+	_clear_message()
 	
 	# Paranoia
 	multiplayer.get_multiplayer_peer().close()
 	
+	var port = _get_port()
+	var max_players = _get_max_players()
+	var ip_address = _get_ip_addr()
+	var maybe_player_name = _get_player_name()
+	
+	if null in [port, max_players, ip_address, maybe_player_name]:
+		return
+	
+	player_name = maybe_player_name as String
+	
 	var peer = ENetMultiplayerPeer.new()
+	peer.set_bind_ip(ip_address)
 	
-	# All sanitization should go here
-	var port := _get_port()
-	var ip_address := _get_ip_addr()
-	var max_players := _get_max_players()
-	player_name = _get_player_name()
-	
-	if not ip_address.is_empty():
-		peer.set_bind_ip(ip_address)
-	
-	var result := peer.create_server(port, max_players)
+	# Subtracting 1 to account for the server, which is a player, but not a client
+	var result := peer.create_server(port, max_players - 1)
 	
 	if result == ERR_CANT_CREATE:
-		message_label.set_text("Could not create the server. Please check if port " + str(port) + " is available.")
-		message_label.set_visible(true)
+		_display_message("Could not create the server. Please check if port " + str(port) + " is available.")
 		return
 	
 	multiplayer.set_multiplayer_peer(peer)
-	
 	_go_to_lobby()
 
 
-func _get_player_name() -> String:
-	var name := name_edit.get_text()
+func _get_player_name():
+	var name := name_edit.get_text().strip_edges()
+	
+	if name.length() == 0:
+		_display_message("Name should not be empty.")
+		return null
+	
+	if name.length() > 20:
+		_display_message("Please keep name at most 20 characters long.")
+		return null
+	
 	return name
 
 
-func _get_port() -> int:
-	var port := int(port_edit.get_text())
+func _get_port():
+	var s_port := port_edit.get_text().strip_edges()
+	
+	if not s_port.is_valid_int():
+		_display_message("Port should be a whole number.")
+		return null
+	
+	var port = int(s_port)
+	
+	if port <= 0:
+		_display_message("Port should be a positive number.")
+		return null
+	
+	if port > 65535:
+		_display_message("Port should not exceed 65535.")
+		return null
+	
 	return port
 
 
-func _get_ip_addr() -> String:
-	var ip_address := ip_addr_edit.get_text()
-	return ip_address
-
-
-func _get_max_players() -> int:
-	# Subtracting 1 to account for the host which seems more intuitive
-	var max_players := int(max_players_edit.get_text()) - 1
+func _get_max_players():
+	var s_max_players = max_players_edit.get_text().strip_edges()
+	
+	if not s_max_players.is_valid_int():
+		_display_message("Max players should be a whole number.")
+		return null
+	
+	var max_players := int(s_max_players)
+	
+	if max_players <= 0:
+		_display_message("A game must contain at least 1 player.")
+		return null
+	
+	# A value of 1 logs an error but apparently works fine
+	
+	if max_players > 4095:
+		_display_message("A game must contain at most 4095 players, unfortunately.")
+		return null
+	
 	return max_players
+
+
+func _get_ip_addr():
+	var ip_address := ip_addr_edit.get_text().strip_edges()
+	
+	if ip_address.is_empty():
+		return "*"
+	
+	if not ip_address.is_valid_ip_address():
+		_display_message("IP address should be a valid IP address.")
+		return null
+	
+	return ip_address
 
 
 func _go_to_lobby():
@@ -72,3 +121,25 @@ func _go_to_lobby():
 	multiplayer_data.self_player = player
 	
 	get_tree().change_scene_to_file(lobby_scene_path)
+
+
+func _clear_message():
+	message_label.set_visible(false)
+	message_label.set_text("")
+
+
+func _display_message(message: String):
+	var current_text = message_label.get_text()
+	
+	if current_text.is_empty():
+		message_label.set_text(message)
+		message_label.set_visible(true)
+	else:
+		message_label.set_text(current_text + "\n" + message)
+	
+	return
+
+
+func _on_return_pressed():
+	# Paranoia
+	multiplayer.get_multiplayer_peer().close()
