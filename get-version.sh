bfe6a8d6c22ec890238f6a213103efc81c196f12
Gabriel Lopes Veiga <veigo@veigo.dev>
2025-12-22 03:47:03 -0300

    Fix get-version script

--- get-version.sh
+++ get-version.sh
@@ -7,7 +7,7 @@ last_tag=`git describe --tags --abbrev=0`
 # + v at the start. Non-capturing groups swapped for capturing ones because it should be more compatible I think.
 tag_regex='^v(0|[1-9]\d*)\.(0|[1-9]\d*)\.(0|[1-9]\d*)(-((0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*)(\.(0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*))*))?(\+([0-9a-zA-Z-]+(\.[0-9a-zA-Z-]+)*))?$'
 
-if ! echo -n "$last_tag" | grep -q -E "$tag_regex"; then
+if ! echo -n "$last_tag" | grep -q -P "$tag_regex"; then
 	echo "Tag '$last_tag' does not match the usual format of a SemVer string prefixed with 'v', like 'v1.2.3'. See <https://semver.org/>. Aborting." >&2
 	exit 1
 fi
